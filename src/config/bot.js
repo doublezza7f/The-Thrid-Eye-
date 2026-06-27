553c4822c2e244f65d43183d1fe44fcf70af90ce
doublezza7f <dadoy20144@gmail.com>
2026-06-27 09:09:15 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The Thrid Eye Is Watching.",
+        name: "The Thrid Eye Is Watching /help ",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
